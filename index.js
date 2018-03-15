213166b3ac307e5108066d6bd1b7edebabe5d773
Danny <kenwai2010@gmail.com>
2018-03-15 22:24:36 +0100

    test video stream

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/video', function(req, res) {
+    var file = path.resolve(__dirname,"assets/sample.mp4");
+    fs.stat(file, function(err, stats) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // 404 Error if file not found
+          return res.sendStatus(404);
+        }
+      res.end(err);
+      }
+      var range = req.headers.range;
+      if (!range) {
+       // 416 Wrong range
+       return res.sendStatus(416);
+      }
+      var positions = range.replace(/bytes=/, "").split("-");
+      var start = parseInt(positions[0], 10);
+      var total = stats.size;
+      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      var chunksize = (end - start) + 1;
+
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+      });
+
+      var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function() {
+          stream.pipe(res);
+        }).on("error", function(err) {
+          res.end(err);
+        });
+    });
+  
 })
 
 module.exports.handler = serverless(app);
